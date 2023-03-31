0f7ec814855a052070feaf1fdaf99fa63a09868e
unw9527 <1041593558@qq.com>
2023-03-31 15:34:31 +0800

    Redraw all text

--- proj/utils.py
+++ proj/utils.py
@@ -48,15 +48,3 @@ def detection_body_parts(landmarks):
         body_parts.loc[i] = lndmrk, cord[0], cord[1]
 
     return body_parts
-
-
-def score_table(exercise, counter, status):
-    score_table = cv2.imread("./images/score_table.png")
-    cv2.putText(score_table, "Activity : " + exercise.replace("-", " "),
-                (10, 65), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (182, 158, 128), 2,
-                cv2.LINE_AA)
-    cv2.putText(score_table, "Counter : " + str(counter), (10, 100),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (182, 158, 128), 2, cv2.LINE_AA)
-    cv2.putText(score_table, "Status : " + str(status), (10, 135),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (182, 158, 128), 2, cv2.LINE_AA)
-    cv2.imshow("Score Table", score_table)
