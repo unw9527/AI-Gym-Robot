b324c059cffe00fc8b190afbb094393eb97d9204
unw9527 <1041593558@qq.com>
2023-05-02 20:05:23 +0800

    functionality done

--- proj/main.py
+++ proj/main.py
@@ -51,11 +51,12 @@ def mouse_callback(event: int, x: int, y: int, flags, param):
         cap.release()
         cv2.destroyAllWindows()
 
-def main(mp_drawing, mp_pose, cap):
+def main(mp_drawing, mp_pose, cap, screenshots):
     with mp_pose.Pose(min_detection_confidence=0.5,
                     min_tracking_confidence=0.5) as pose:
         counter = 0 
         status = True  # State of move
+        start_time = datetime.now()
         
         while cap.isOpened():
             success, frame = cap.read()
@@ -75,8 +76,13 @@ def main(mp_drawing, mp_pose, cap):
 
             try:
                 landmarks = results.pose_landmarks.landmark
-                counter, status = TypeOfExercise(landmarks).calculate_exercise(
+                counter, status, msg = TypeOfExercise(landmarks).calculate_exercise(
                     args["exercise_type"], counter, status)
+                if msg:
+                    cv2.rectangle(frame, (50, 50), (750, 150), (0, 255, 255), cv2.FILLED)
+                    cv2.putText(frame, msg, (60, 100),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2, cv2.LINE_AA)
+                    cv2.imwrite(os.path.join(screenshots, f"{now.strftime('%Y-%m-%d-%H-%M')}.jpg"), frame)
             except:
                 pass
             
@@ -86,13 +92,6 @@ def main(mp_drawing, mp_pose, cap):
             cv2.rectangle(frame, (700, 0), (800, 40), button_color, -1, cv2.LINE_AA)
             cv2.putText(frame, "Exit", (720, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, button_text_color, 2, cv2.LINE_AA)
 
-
-            # cv2.rectangle(frame, (20, 200), (780, 350), (0, 255, 255), cv2.FILLED)
-            # cv2.putText(frame, "Warning: More than 1 person detected!", (30, 250),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2, cv2.LINE_AA)
-            # cv2.putText(frame, "Please leave only 1 person in the view", (30, 300),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2, cv2.LINE_AA)
-                
             # Top left of image
             cv2.rectangle(frame, (0, 0), (200, 40), (255, 255, 255), cv2.FILLED)
             cv2.putText(frame, "Activity: " + args["exercise_type"].replace("-", " "),
@@ -127,6 +126,7 @@ def main(mp_drawing, mp_pose, cap):
                 break
         
         logger.info(f"Total number of repetitions: {counter}")
+        logger.info(f"Used time: {datetime.now() - start_time}")
         logger.info("Finish exercising. Thanks for using Gym Robot Trainer!")
         cap.release()
         cv2.destroyAllWindows()
@@ -150,6 +150,10 @@ if __name__ == "__main__":
     if not os.path.exists(log_folder):
         os.makedirs(log_folder)
         
+    screenshots = "screenshots"
+    if not os.path.exists(screenshots):
+        os.makedirs(screenshots)
+        
     now = datetime.now()
     log_file = os.path.join(log_folder, f'feedback-{now.strftime("%Y-%m-%d-%H-%M")}.log')
     logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
@@ -167,8 +171,8 @@ if __name__ == "__main__":
     else:
         cap = cv2.VideoCapture(0)  # webcam
         logger.info("Video source: webcam")
-
+    
     cap.set(3, 800)  # width
     cap.set(4, 480)  # height
     
-    main(mp_drawing, mp_pose, cap)
+    main(mp_drawing, mp_pose, cap, screenshots)
--- proj/types_of_exercise.py
+++ proj/types_of_exercise.py
@@ -27,12 +27,12 @@ class TypeOfExercise(BodyPartAngle):
             status (bool): whether the push-up is finished
 
         Returns:
-            list: [counter, status]
+            list: [counter, status, msg]
         """
-        self.check_body_position()
         elbow_angle = self.angle_of_elbow()
         shoulder_angle = self.angle_of_shoulder()
         hip_angle = self.angle_of_hip()
+        msg = ''
         
         # Percentage of success of pushup
         per = np.interp(elbow_angle, (90, 160), (0, 100))
@@ -42,7 +42,7 @@ class TypeOfExercise(BodyPartAngle):
         
         # You cannot do push-up while standing up
         if self.l_hip[1] - self.l_shoulder[1] > 0.1:
-            return [counter, status]
+            return [counter, status, msg]
         
         if per == 0:
             if not status:
@@ -53,15 +53,16 @@ class TypeOfExercise(BodyPartAngle):
                 else:
                     if elbow_angle > 90 and 10 < shoulder_angle <= 40 and hip_angle > 150:
                         logger.warning(f"Push-up {counter + 1}: Your elbow angle is too large")
+                        msg += "Elbow angle too large. "
                     if 10 < elbow_angle <= 90 and shoulder_angle > 40 and hip_angle > 150:
                         logger.warning(f"Push-up {counter + 1}: Your shoulder angle is too large")
+                        msg += "Shoulder angle too large. "
                     if 10 < elbow_angle <= 90 and 10 < shoulder_angle <= 40 and hip_angle <= 150:
                         logger.warning(f"Push-up {counter + 1}: Your hip angle is too small")
-        else:
-            if elbow_angle > 90 and shoulder_angle > 40 and hip_angle > 150:
-                if status:
-                    status = False
-        return [counter, status]
+                        msg += "Hip angle is too small. "
+        elif elbow_angle > 90 and shoulder_angle > 40 and hip_angle > 150 and status:
+            status = False
+        return [counter, status, msg]
 
     def squat(self, counter: int, status: bool) -> list:
         """Check whether a squat is done and count the number of squats
@@ -71,23 +72,29 @@ class TypeOfExercise(BodyPartAngle):
             status (bool): whether the squat is finished
 
         Returns:
-            list: [counter, status]
+            list: [counter, status, msg]
         """
-        # print(self.l_ankle[1], self.r_ankle[1])
         # You are not doing squat if one of your feet is up
         if abs(self.l_ankle[1] - self.r_ankle[1]) > 0.1:
             return [counter, status]
         
         knee_angle = self.angle_of_knee()
         hip_angle = self.angle_of_hip()
-        # print(knee_angle, hip_angle)
+        msg = ''
         
         if knee_angle > 169 and hip_angle > 160:
             status = False
-        if 0 < knee_angle <= 120 and 0 < hip_angle <= 120 and status == False: # the knee angle is fine-tuned
-            status = True
-            counter += 1
-        return [counter, status]
+        if not status:
+            if 120 < knee_angle <= 169 and 0 < hip_angle <= 120:
+                logger.warning(f"Squat {counter + 1}: Your knee angle is too large")
+                msg += "Knee angle too large. "
+            if 0 < knee_angle <= 120 and hip_angle > 120:
+                logger.warning(f"Squat {counter + 1}: Your hip angle is too large")
+                msg += "Hip angle too large. "
+            if 0 < knee_angle <= 120 and 0 < hip_angle <= 120: # the knee angle is fine-tuned
+                status = True
+                counter += 1
+        return [counter, status, msg]
 
     def calculate_exercise(self, exercise_type: str, counter: int, status: bool) -> list:
         """Call the corresponding function for the exercise type specified in args
@@ -98,18 +105,11 @@ class TypeOfExercise(BodyPartAngle):
             status (bool): whether the push-up/squat is finished
 
         Returns:
-            list: [counter, status]
+            list: [counter, status, msg]
         """
         if exercise_type == "push-up":
-            counter, status = TypeOfExercise(self.landmarks).push_up(counter, status)
+            counter, status, msg = TypeOfExercise(self.landmarks).push_up(counter, status)
         elif exercise_type == "squat":
-            counter, status = TypeOfExercise(self.landmarks).squat(counter, status)
-        return [counter, status]
+            counter, status, msg = TypeOfExercise(self.landmarks).squat(counter, status)
+        return [counter, status, msg]
     
-    def check_body_position(self):
-        """Check whether all body parts can be seen
-        """
-        # for joint in self.body_parts:
-        #     if self.body_parts[joint][2] < 0.7:
-        #         print(f"Cannot see your {joint}!") # TODO add warning sound
-        pass
