9e2d3f5c3ab566ab470949e0c177847bcf85cb8d
unw9527 <1041593558@qq.com>
2023-05-02 19:06:42 +0800

    add: requirements; clean up script

---
+++ proj/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# Author: Kunle Li
+# Creation Date: 2023-05-02
+# Description: Remove log files and videos files in batches
+# ------------------------------------------------------------------
+
+# Remove log files
+rm -r ./logs
+rm -r ./output_videos
+rm -r ./screenshots
+
